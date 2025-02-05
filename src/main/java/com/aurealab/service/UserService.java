32eb28873a93a62dde3b6f86190a4f768145d713
WillCast <williamisrael210@gmail.com>
2025-02-05 17:22:43 -0500

    awesome reboot in the master branch

--- src/main/java/com/aurealab/service/UserService.java
+++ src/main/java/com/aurealab/service/UserService.java
@@ -8,5 +8,6 @@ import java.util.List;
 public interface UserService {
     public APIResponseDTO<List<UserDTO>> getUsers(int itemsPerPage, int activePage);
     public APIResponseDTO<UserDTO> getUser(Long id);
+    
     public APIResponseDTO<String> saveUser(UserDTO user);
 }
